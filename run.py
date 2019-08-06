b7c4ca9462648873c00934b82088f88a51529a73
Ubuntu <ubuntu@ip-172-31-86-242.ec2.internal>
2019-08-06 16:13:55 +0000

    Started accordian menu layout

--- run.py
+++ run.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
-
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+    
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'recipe_db'
+app.config['MONGO_URI'] = 'mongodb+srv://root:r00tUser@myfirstcluster-tbkzy.mongodb.net/recipe_db?retryWrites=true&w=majority'
 
+mongo = PyMongo(app) #constructor method
+
+#route decorator is a URL that redirects to a Python function
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_recipes')
+def get_tasks():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
