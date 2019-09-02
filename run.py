af53a8438b8e5707dc162ca8b342444f07723637
Ubuntu <ubuntu@ip-172-31-86-242.ec2.internal>
2019-09-02 16:28:36 +0000

    Ability to upload an image and display. Bug with loop only displaying img of first item in Recipe list

--- run.py
+++ run.py
@@ -12,11 +12,16 @@ app.config['MONGO_URI'] = 'mongodb+srv://root:r00tUser@myfirstcluster-tbkzy.mong
 
 mongo = PyMongo(app) #constructor method
 
+@app.route('/file/<filename>')
+def file(filename):
+    return mongo.send_file(filename)
+
 #route decorator is a URL that redirects to a Python function
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
-    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+    return render_template("recipes.html", recipes=mongo.db.recipes.find(), files=mongo.db.fs.files.find())
+
 
 @app.route('/new_recipe')
 def new_recipe():
@@ -26,8 +31,20 @@ def new_recipe():
 @app.route('/create_recipe', methods=['POST'])
 def create_recipe():
     recipes = mongo.db.recipes
-    recipes.insert_one(request.form.to_dict())
-    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+    this_recipe = recipes.insert_one(request.form.to_dict())
+    recipe_id = this_recipe.inserted_id
+    profile_image = request.files['profile_image']
+    mongo.save_file(profile_image.filename, profile_image, base='fs', content_type = 'image', recipe_id = recipe_id )
+    return render_template("recipes.html", recipes=mongo.db.recipes.find(), files=mongo.db.fs.files.find())
+
+# @app.route('/create_image', methods=['POST'])
+# def create_image():
+#     recipes = mongo.db.recipes
+#     if 'profile_image' in request.files:
+#         profile_image = request.files['profile_image']
+#         mongo.save_file(profile_image.filename, profile_image)
+#         mongo.db.recipes.insert({'profile_image_name' : profile_image_name})
+#         return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
 @app.route('/edit_recipe/<recipe_id>')
@@ -67,6 +84,7 @@ def create_ingredient():
     ingredients = mongo.db.ingredients
     ingredients.insert_one(request.form.to_dict())
     return render_template("ingredients.html", ingredients=mongo.db.ingredients.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
