a7085983a698871966b2b9aebf5de7152e138468
Neil Larkin <47116610+neillarkin@users.noreply.github.com>
2021-03-27 18:10:28 +0000

    Updated gitignore

---
+++ config.py
@@ -0,0 +1,6 @@
+usr = 'root'
+pw = 'r00tUser'
+
+mongodb = 'recipe_db'
+uri = 'mongodb+srv://' + usr + ':' + pw + '@myfirstcluster-tbkzy.mongodb.net/recipe_db?retryWrites=true&w=majority'
+# uri= 'mongodb+srv://root:r00tUser@myfirstcluster-tbkzy.mongodb.net/recipe_db?retryWrites=true&w=majority'
